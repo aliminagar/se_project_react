98ea68bbf4514f2a15c81d636b81f3526d0a7ad7
Alireza Minagar <127906160+aliminagar@users.noreply.github.com>
2025-06-16 23:03:59 -0500

    Delete WTWR/se_project_express directory

--- WTWR/se_project_express/routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const authMiddleware = require("../middlewares/auth");
-const { getCurrentUser, updateUser } = require("../controllers/users");
-
-router.get("/me", authMiddleware, getCurrentUser);
-router.patch("/me", authMiddleware, updateUser);
-
-module.exports = router;
